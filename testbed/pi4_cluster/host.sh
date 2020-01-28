9e74b7bf660b7dcccf3c0e9be764ced5f579453e
hikikones <hikikones@gmail.com>
2020-01-28 18:53:07 +0100

    Use single quotes instead in route setup

--- testbed/pi4_cluster/host.sh
+++ testbed/pi4_cluster/host.sh
@@ -38,6 +38,6 @@ sudo nano /etc/resolve.conf
 
 # Add route on interface up
 sudo touch /etc/network/if-up.d/route
-sudo echo "#!/bin/sh" >> /etc/network/if-up.d/route
-sudo echo "ip route add 192.168.xx.0/24 via 192.168.xx.1" >> /etc/network/if-up.d/route
+sudo echo '#!/bin/sh' >> /etc/network/if-up.d/route
+sudo echo 'ip route add 192.168.xx.0/24 via 192.168.xx.1' >> /etc/network/if-up.d/route
 sudo chmod +x /etc/network/if-up.d/route
